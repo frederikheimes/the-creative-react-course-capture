ae139ed80b2553f14139b6be0bdd9add65c7b589
Frederik Luca Heimes <heimesfrederik@gmail.com>
2023-08-02 20:46:17 +0200

    4. Sercives Section

---
+++ src/Components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+              corrupti!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+              corrupti!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+              corrupti!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What Products Do You Offer?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+              corrupti!
+            </p>
+          </div>
+        </div>
+      </h2>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,17 @@
 import React from "react";
 //Page Components
 import AboutSection from "../Components/AboutSection";
+import ServicesSection from "../Components/ServicesSection";
+import FaqSection from "../Components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
